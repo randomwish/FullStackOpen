3e6355436ecc4458b5e76a784e111eef2b2e1ac0
Yenzhe Wee <13531318+randomwish@users.noreply.github.com>
2024-01-09 01:49:23 +0800

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   const { showAll, setShowAll, filterElement, handleFilterChange } = props;
@@ -53,18 +54,29 @@ const Persons = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  // ]);
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newID, setNewID] = useState(persons.length + 1);
   const [filterElement, setFilterElement] = useState("");
   const [showAll, setShowAll] = useState(true);
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+        const persons = response.data
+        console.log(persons)
+        setPersons(persons)
+    })
+  },[])
+  
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
